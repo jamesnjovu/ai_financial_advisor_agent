5b99f6ad1236c67c91139230f5e58aaac91228fe
James <njovujames@gmail.com>
2025-06-09 10:33:19 +0200

    feat: Add check_origin configuration for live view in endpoint

--- config/config.exs
+++ config/config.exs
@@ -27,7 +27,12 @@ config :app, AppWeb.Endpoint,
     layout: false
   ],
   pubsub_server: App.PubSub,
-  live_view: [signing_salt: "k3f1zvC6"]
+  live_view: [signing_salt: "k3f1zvC6"],
+  check_origin: [
+    "//66.42.87.179:4001",
+    "//ai-financial-advisor-agent.onrender.com",
+    "//aifinancialagent.sms.probasegroup.com",
+  ]
 
 
 # OAuth configs
